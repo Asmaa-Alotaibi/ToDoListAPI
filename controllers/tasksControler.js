28fc9405bb4271f1bc90f19a1f81027f591f920b
Dalal Hamadi <dalalhamadi@Dalals-MBP.hotspot>
2020-11-24 17:46:06 +0300

    packend

--- controllers/tasksControler.js
+++ controllers/tasksControler.js
@@ -1,3 +1,10 @@
 let tasks = require("../tasks");
 
 exports.tasksList = (req, res) => res.json(tasks);
+
+exports.createTask = (req, res) => {
+  const id = tasks[tasks.length - 1].id + 1;
+  const newTask = { id, ...req.body };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { tasksList } = require("../controllers/tasksControler");
+const { tasksList, createTask } = require("../controllers/tasksControler");
 
 /*  get tasks*/
 router.get("/", tasksList);
 
 /* create task*/
-
+router.post("/", createTask);
 /* delete task*/
 
 module.exports = router;
